b1524db9b7fe5f604bb9e6d5d0558dd5dcdbaddf
Patrick Thibaudeau <thibaud@yorku.ca>
2023-10-11 21:04:04 -0400

    Fixed issue with update bot. Call had to be config_bulk

--- classes/gpt.php
+++ classes/gpt.php
@@ -55,7 +55,6 @@ class gpt
         }
         $result = json_decode(curl_exec($ch));
         curl_close($ch);
-        file_put_contents('/var/www/fakesmartsdata/temp/data.json', json_encode($result));
         return $result;
     }
 
