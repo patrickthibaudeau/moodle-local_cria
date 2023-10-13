1485e0e23a24c172b3d1b5f853150b0da65c15de
Patrick Thibaudeau <thibaud@yorku.ca>
2023-10-12 21:05:49 -0400

    Fixed URL

--- amd/src/bot_config.js
+++ amd/src/bot_config.js
@@ -46,7 +46,7 @@ function share_bot() {
         let share = $(this).data('share');
         let theme = $(this).data('theme');
         theme = theme.replace(/\#/g, "");
-        $('#share-code').html(`<code> \t&lt;script type="text/javascript" src="${share}/embed?botId=${bot_id}&theme=${theme}" async&gt; \t&lt;/script&gt;</code>`);
+        $('#share-code').html(`<code> \t&lt;script type="text/javascript" src="${share}/embed?botId=${bot_id}&embedTheme=${theme}" async&gt; \t&lt;/script&gt;</code>`);
         $('#shareModal').modal('show');
     });
 }
