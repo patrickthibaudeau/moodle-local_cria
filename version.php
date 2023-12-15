2434f65b95b7036154a676b2037f44f5dedbc582
Patrick Thibaudeau <thibaud@yorku.ca>
2023-12-15 15:20:34 -0500

    Upgraded version

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
-$plugin->version = 2023121500;
+$plugin->version = 2023121501;
 // Moodle version required
 $plugin->requires = 2022112800; //Moodle 4.1
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '0.2.0';
+$plugin->release = '0.2.2';
 $plugin->component = 'local_cria';
