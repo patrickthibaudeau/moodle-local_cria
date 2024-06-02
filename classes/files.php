d51ec0945049dbed963b9fad243b4b51b517ae3c
Patrick Thibaudeau <thibaud@yorku.ca>
2024-06-02 10:00:42 -0400

    Added comments

--- classes/files.php
+++ classes/files.php
@@ -74,10 +74,17 @@ class files {
      */
     public function publish_all_files() {
         global $CFG;
+
+        // Create a new instance of the Intent class with the current intent_id
         $INTENT = new intent($this->intent_id);
+        // Create a new instance of the Criaparse class
         $PARSER = new criaparse();
+        // Create a new instance of the File class
         $FILE = new file();
+        // Create a new instance of the Bot class with the bot_id from the current intent
         $BOT = new bot($INTENT->get_bot_id());
+
+// Get the bot name from the current intent
         $bot_name = $INTENT->get_bot_name();
         // set bot parsing strategy
         $bot_parsing_strategy = $BOT->get_parse_strategy();
@@ -105,12 +112,14 @@ class files {
                 $file->copy_content_to($file_path . '/' . $file->get_filename());
                 // Set parsing strategy based on file type.
                 $parsing_strategy = $PARSER->set_parsing_strategy_based_on_file_type($file->get_mimetype(), $bot_parsing_strategy);
+                // Get parsed results
                 $results = $PARSER->execute($parsing_strategy, $file_path . '/' . $file->get_filename());
                 // Delete file from CriaBot
                 criabot::document_delete($bot_name, $file->get_filename());
                 if ($results['status'] != 200) {
                     \core\notification::error('Error parsing file: ' . $results['message']);
                 } else {
+                    // Get nodes from parsed results
                     $nodes = $results['nodes'];
                     // Send nodes to indexing server
                     $upload = $FILE->upload_nodes_to_indexing_server($bot_name, $nodes, $file->get_filename(), $FILE->get_file_type_from_mime_type($file->get_mimetype()), false);
